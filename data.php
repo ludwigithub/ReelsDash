e2037bf2ba2487e7b21b4bf93ffb2e5c91ecefe5
asarogers <cyberasasoftware@gmail.com>
2022-07-01 22:15:44 -0500

    testing testing

--- data.php
+++ data.php
@@ -1,17 +1,3 @@
 <?php
-function error($msg){
-    $response = array("success" => false, "message" => $msg);
-    return json_encode($response);
-}
-$name = $_POST['name'];
-
-if($name ==''){
-    die(error("Error: No name"));
-}
-
-$name = 'Asa';
-$messaage = "Created" . $name;
-$response =array();
-$response["success"] = true;
-$response["message"] = $message;
+echo "this is the data";
 ?>
