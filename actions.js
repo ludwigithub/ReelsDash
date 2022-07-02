cf7f2e248bd933ee27683b455b3f2d088116fa4f
asarogers <cyberasasoftware@gmail.com>
2022-07-02 18:06:41 -0500

    testing testing

--- actions.js
+++ actions.js
@@ -2,8 +2,8 @@ $(document).ready(function () {
     var tit = ["Text 1", "Text 2", "Text 3", "Text 4", "Text 5"];
 
     for (var i in tit) {
-        var element = document.createElement("h2");
+        var element = document.createElement("a");
         element.innerHTML = tit[i];
-        $("#tit").append(element);
+        $("#greeting").append(element);
     }
 });
