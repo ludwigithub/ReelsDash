62ae55854de424e655c55caa4454279eb316445f
asarogers <cyberasasoftware@gmail.com>
2022-07-03 13:08:22 -0500

    testing testing

--- actions.js
+++ actions.js
@@ -25,7 +25,7 @@ function generateTable(table, data) {
     for (key in element) {
       let cell = row.insertCell();
       if(element[key] ==  element["Line"]){
-        cell.className = ".colName";
+        cell.className = "colName";
       }
       let text = document.createTextNode(element[key]);
       cell.appendChild(text);
