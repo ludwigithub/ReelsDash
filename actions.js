6e2ab8e6f9bc257aa064c8b84c7a947d3edb70db
asarogers <cyberasasoftware@gmail.com>
2022-07-03 12:43:33 -0500

    testing testing

--- actions.js
+++ actions.js
@@ -23,7 +23,7 @@ function generateTable(table, data) {
   for (let element of data) {
     let row = table.insertRow();
     for (key in element) {
-        alert(element[key]);
+        console.log(element[key]);
       let cell = row.insertCell();
       let text = document.createTextNode(element[key]);
       cell.appendChild(text);
