cab735564409d41e48ffe318a7c0e9a12d2e733e
asarogers <cyberasasoftware@gmail.com>
2022-07-02 21:50:08 -0500

    testing testing

--- actions.js
+++ actions.js
@@ -8,3 +8,18 @@ $(document).ready(function () {
         $("#greeting").append(element);
     }
 });
+
+let tableHeaders = ["Global Ranking", "Username", "Score", "Time Alive [seconds]", "Accuracy [%]"]
+
+let scoreboardTable = document.createElement('table') // Create the table itself
+scoreboardTable.className = 'scoreboardTable'
+let scoreboardTableHead = document.createElement('thead') // Creates the table header group element
+scoreboardTableHead.className = 'scoreboardTableHead'
+let scoreboardTableHeaderRow = document.createElement('tr') // Creates the row that will contain the headers
+scoreboardTableHeaderRow.className = 'scoreboardTableHeaderRow'
+
+tableHeaders.forEach(header => {
+let scoreHeader = document.createElement('th') // Creates the current header cell during a specific iteration
+scoreHeader.innerText = header
+scoreboardTableHeaderRow.append(scoreHeader) // Appends the current header cell to the header row
+})
