fbbd34c755a0535f96654b59952a3a3e9b93d89c
asarogers <cyberasasoftware@gmail.com>
2022-07-03 12:29:39 -0500

    testing testing

--- actions.js
+++ actions.js
@@ -36,6 +36,6 @@ let data = Object.keys(mountains[0]);
 generateTableHead(table, data);
 generateTable(table, mountains);
 
-var tr = document.querySelector('table').tHead;
-tr.insertCell(1).outerHTML = "<th>Second</th>"
+var tr = document.querySelector('table').t.children[0];
+tr.insertCell(0).outerHTML = "<th>Second</th>"
 });
