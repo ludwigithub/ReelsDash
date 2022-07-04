830490094767a04ca810756d28dff390ad371267
Asa Rogers <71350584+asarogers@users.noreply.github.com>
2022-07-04 16:06:45 -0500

    make it better

--- dbh.php
+++ dbh.php
@@ -13,8 +13,17 @@ $password = "Reels.Ace";
 $server = "MXL1072KZ8";
 // Opens a connection to a MySQL server
 
+
+
+$link = mysql_connect("MXL1072KZ8", "ace", "Reels.Ace");
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
+}
+echo 'Connected successfully';
+
+mysql_close($link);
+
 echo json_encode($name);
-$connection = mysql_connect($server, $username, $password);
 //if you want to suppress the error message, substitute the connection line for:
 //$connection = @mysql_connect($server, $username, $password) or die('try again in some minutes, please');
 
