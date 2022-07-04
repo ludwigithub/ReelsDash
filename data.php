5906b97333c8d96b5213ee4bcab0f7b45b0f8161
Asa Rogers <71350584+asarogers@users.noreply.github.com>
2022-07-04 14:14:51 -0500

    make it better

--- data.php
+++ data.php
@@ -10,4 +10,14 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
   }
  echo "Connected successfully";
+
+ function debug_to_console($data) {
+    $output = $data;
+    if (is_array($output))
+        $output = implode(',', $output);
+
+    echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+}
+
+debug_to_console("Test");
 ?>
