2c55e88f714092859a8387affbcc4910f3124da7
asarogers <cyberasasoftware@gmail.com>
2022-07-03 20:24:11 -0500

    testing testing

--- actions.js
+++ actions.js
@@ -9,7 +9,8 @@ let mountains = [
   {  Line: "Monte Falco", Shift: 1658, "Units Produced": "Parco Foreste Casentinesi", Uptime: 1, "Avg Speed": 2, "Speed(10min)": 3, "Data Integrity": 4, "Order Info": 5, Need: 6 },
   {  Line: "Monte Falco1", Shift: 1659, "Units Produced": "Parco Foreste Casentinesi1", Uptime: 1, "Avg Speed": 2, "Speed(10min)": 3, "Data Integrity": 4, "Order Info": 5, Need: 6 }
 ];
-mountains["Line"].push("yes");
+alert(mountains["Line"][0]);
+alert(mountains["Line"][1]);
 
 
 function generateTableHead(table, data) {
