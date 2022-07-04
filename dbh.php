b4e8d26bd9185d2129321c6b2f60a31489d9980a
Asa Rogers <71350584+asarogers@users.noreply.github.com>
2022-07-04 15:53:08 -0500

    make it better

--- dbh.php
+++ dbh.php
@@ -3,13 +3,19 @@ $name = "David";
 
 header('Content-Type: application/json');
 
-echo json_encode($name);
-/*
+
+
 $server = "MXL1072KZ8";
 $username = "ace";
 $password = "Reels.Ace";
 $dbname = "dashboard";
 
-$conn = mysqli_connect($server, $username, $password, $dbname);
+//$conn = mysqli_connect($server, $username, $password, $dbname);
 
-*/?>
+$conn = new mysqli($server, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die(echo json_encode("Connection failed: ") . $conn->connect_error);
+ }
+ echo json_encode("Connected successfully");
+ ?>
