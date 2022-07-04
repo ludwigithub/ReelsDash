b9ac1a5a3c9cbae76b6f6b31c30b7dc9ad844ef2
Asa Rogers <71350584+asarogers@users.noreply.github.com>
2022-07-04 15:30:15 -0500

    make it better

---
+++ dbh.php
@@ -0,0 +1,9 @@
+<?php
+$server = "MXL1072KZ8";
+$username = "ace";
+$password = "Reels.Ace";
+$dbname = "dashboard";
+
+$conn = mysqli_connect($server, $username, $password, $dbname);
+
+echo "it works";
