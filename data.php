c8d13b907a8bee11029c01996e9d8c67eb545f40
Asa Rogers <71350584+asarogers@users.noreply.github.com>
2022-07-04 14:16:40 -0500

    make it better

--- data.php
+++ data.php
@@ -1,5 +1,15 @@
 <?php
 
+function debug_to_console($data) {
+    $output = $data;
+    if (is_array($output))
+        $output = implode(',', $output);
+
+    echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+}
+
+debug_to_console("Test");
+
 $servername = "MXL1072KZ8";
 $username = "ace";
 $password = "Reels.Ace";
@@ -11,13 +21,8 @@ if ($conn->connect_error) {
   }
  echo "Connected successfully";
 
- function debug_to_console($data) {
-    $output = $data;
-    if (is_array($output))
-        $output = implode(',', $output);
 
-    echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
-}
+ 
 
-debug_to_console("Test");
+
 ?>
