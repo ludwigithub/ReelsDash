34892d7868261ac70e8dc511cd3548d30c75b92f
Asa Rogers <71350584+asarogers@users.noreply.github.com>
2022-07-06 08:24:56 -0500

    there wew go

--- config.php
+++ config.php
@@ -5,3 +5,5 @@ $db_pass = "f81aeb52";
 $db_name = "heroku_671f81d6999c808";
 
 $connect = mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die("database connection error");
+
+echo "Success";
