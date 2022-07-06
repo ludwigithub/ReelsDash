cbe67abe081a087df1871647763f8ce21ea06199
Asa Rogers <71350584+asarogers@users.noreply.github.com>
2022-07-06 08:26:31 -0500

    there wew go

--- config.php
+++ config.php
@@ -6,4 +6,13 @@ $db_name = "heroku_671f81d6999c808";
 
 $connect = mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die("database connection error");
 
-echo "Success";
+
+$sql = "SELECT orderindex from dashinfo;";
+        $result = mysqli_query($pdo, $sql);
+        $check = mysqli_num_rows($result);
+        if($check > 0){
+            while($row = mysqli_fetch_assoc($result)){
+                echo json_encode(1);
+
+            }
+        }
