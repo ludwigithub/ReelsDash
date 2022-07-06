3f6252df3ac8f4ed0a046c2e3989dce2bc484d5e
Asa Rogers <71350584+asarogers@users.noreply.github.com>
2022-07-06 08:22:49 -0500

    there wew go

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$db_host = "f81aeb52@us-cdbr-east-06.cleardb.net";
+$db_user = "be3e1e29137712";
+$db_pass = "f81aeb52";
+$db_name = "heroku_671f81d6999c808";
+
+$connect = mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die("database connection error");
